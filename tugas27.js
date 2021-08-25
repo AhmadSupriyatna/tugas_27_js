94a656e69382b14567401648994e1f6994a6c527
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-26 05:15:02 +0700

    Tugas 27 Javascript
    Tugas ini kami kumpukan untuk Niomic

---
+++ tugas27.js
@@ -0,0 +1,16 @@
+function tugasDuaTujuh(){
+    var a = "No ";
+    var b = "rumah saya ";
+    var c = 30;
+    var d = " A";
+
+    var result = a + b + String(c)+ d;
+    console.log(result);
+
+    var e = "Jalan pangeran Tirtayasa No ";
+    var f = 70
+
+    return e + String(f);
+    
+}
+console.log(tugasDuaTujuh())
